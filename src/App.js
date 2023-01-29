08f59a50a1e25f4363a12b50b02157fefa9804df
Rocky\rg383 <rg383@njit.edu>
2023-01-28 23:27:43 -0500

    feat: created initial layout

--- src/App.js
+++ src/App.js
@@ -1,66 +1,153 @@
-// import logo from './logo.svg';
- import './App.css';
+import "./App.css";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import CssBaseline from '@mui/material/CssBaseline';
+import React, { useRef } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
 
 export default function ImgMediaCard() {
   return (
     <React.Fragment>
-    <CssBaseline />
-    <Container maxWidth="sm">
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image="/logo512.png"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
-    </Container>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Grid
+          container
+          wrap="nowrap"
+          spacing={2}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs>
+            <Typography variant="h2" component="h2" className="page-title">
+              Preeti's Referral Links
+            </Typography>
+          </Grid>
+          <Grid item xs>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image="/logo512.png"
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  className="app-title"
+                >
+                  TikTok
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  className="app-details"
+                >
+                  TikTok is a popular social media platform which is majorly
+                  based on users viewing, creating and sharing short form videos
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Link
+                  href="https://www.google.com"
+                  underline="hover"
+                  target="_blank"
+                  className="app-link"
+                >
+                  Get 25$ when you install TikTok
+                </Link>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image="/logo512.png"
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  className="app-title"
+                >
+                  Flip
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  className="app-details"
+                >
+                  Flip is a popular social media platform which is majorly
+                  based on users viewing, creating and sharing short form videos
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Link
+                  href="https://www.google.com"
+                  underline="hover"
+                  target="_blank"
+                  className="app-link"
+                >
+                  Get 25$ when you install Flip
+                </Link>
+              </CardActions>
+            </Card>
+          </Grid>
+
+          <Grid item xs>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="green iguana"
+                height="140"
+                image="/logo512.png"
+              />
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  className="app-title"
+                >
+                  DoorDash
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  className="app-details"
+                >
+                  DoorDash is a popular social media platform which is majorly
+                  based on users viewing, creating and sharing short form videos
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Link
+                  href="https://www.google.com"
+                  underline="hover"
+                  target="_blank"
+                  className="app-link"
+                >
+                  Get 25$ when you install DoorDash
+                </Link>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
     </React.Fragment>
   );
 }
